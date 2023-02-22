c1d5667649337feb5635562693b0a17cdffe0d2b
Henrytoo <90352329+Henrytoo@users.noreply.github.com>
2023-02-21 17:32:09 -0800

    fixed minor issues

--- src/main/java/frc/robot/commands/DriveDistance.java
+++ src/main/java/frc/robot/commands/DriveDistance.java
@@ -8,15 +8,15 @@ public class DriveDistance extends CommandBase {
     public DriveTrainSubsystem driveTrain;
 
     public DriveDistance(Double distance,DriveTrainSubsystem driveTrain){
-        this.distance = (distance/((0.1524*Math.PI)/(10.71)))*2048;
+        this.distance = (distance/((0.1524*Math.PI)/(10.71)))*2048;//= (distance traveled / (circumference of driven wheel or pulley / gear ratio)) * encoder counts per revolution
         this.driveTrain = driveTrain;
     }
 
 
     public void execute() {
-        driveTrain.setMotorPowers(0.5, 0.5,"Fred");
+        driveTrain.setMotorPowers(0.5, 0.5,"Motor go Burrr");
         if (driveTrain.leftDriveFalconFront.getCurrentEncoderValue() > distance-200 && driveTrain.leftDriveFalconFront.getCurrentEncoderValue() < distance+200){
-            driveTrain.setMotorPowers(0, 0,"joe");
+            driveTrain.setMotorPowers(0, 0,"Motor stop now (:");
         }
     }
 }
