8b814ff679aaf16b2165386644483ffa3944340a
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2023-03-04 12:27:10 -0500

    Added manual elevator control

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -1,13 +1,13 @@
 package frc.robot;
 
 public class Ports {
-   public final static int RIGHT_DRIVE_FALCON_FRONT = 3;
+   public final static int RIGHT_DRIVE_FALCON_FRONT = 2;
    public final static int RIGHT_DRIVE_FALCON_BACK = 4;
-   public final static int LEFT_DRIVE_FALCON_FRONT = 1;
-   public final static int LEFT_DRIVE_FALCON_BACK = 2;
+   public final static int LEFT_DRIVE_FALCON_FRONT = 3;
+   public final static int LEFT_DRIVE_FALCON_BACK = 1;
    public final static int INTAKE_MOTOR = 19;
-   public final static int ELEVATOR_MOTOR_MAIN = 10; //Remove sub
-   public final static int ELEVATOR_MOTOR_SUB = 0;
+   public final static int ELEVATOR_MOTOR_MAIN = 18;
+   // public final static int ELEVATOR_MOTOR_SUB = 0;
    public final static int CLAW_CLOSE = 0; //Do these later
    public final static int CLAW_OPEN = 0;
 }
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -11,7 +11,7 @@ import frc.robot.TeamTalonFX;
 public class ElevatorSubsystem extends SubsystemBase {
     
     public ITeamTalon elevatorMotorMain;
-    public ITeamTalon elevatorMotorSub;
+    // public ITeamTalon elevatorMotorSub;
     public double motorSpeed = Constants.elevatorMotorSpeed;
     public double targetPosition;
     public final double DEADBAND_VALUE = Constants.ELEVATOR_DEADBAND_VALUE;
@@ -19,27 +19,38 @@ public class ElevatorSubsystem extends SubsystemBase {
 
     public ElevatorSubsystem() {
         elevatorMotorMain = new TeamTalonFX("Subsystem.Elevator.ElevatorMotorMain", Ports.ELEVATOR_MOTOR_MAIN);
-        elevatorMotorSub = new TeamTalonFX("Subsystem.Elevator.ElevatorMotorSub", Ports.ELEVATOR_MOTOR_SUB);
+        // elevatorMotorSub = new TeamTalonFX("Subsystem.Elevator.ElevatorMotorSub", Ports.ELEVATOR_MOTOR_SUB);
 
         elevatorMotorMain.enableVoltageCompensation(true);
-        elevatorMotorSub.enableVoltageCompensation(true);
+        // elevatorMotorSub.enableVoltageCompensation(true);
 
         elevatorMotorMain.setNeutralMode(NeutralMode.Brake);
-        elevatorMotorSub.setNeutralMode(NeutralMode.Brake);
+        // elevatorMotorSub.setNeutralMode(NeutralMode.Brake);
 
-        elevatorMotorSub.follow(elevatorMotorMain);
+        // elevatorMotorSub.follow(elevatorMotorMain);
     }
     public void setTargetPosition(double newTargetPosition, String reason) {
         targetPosition = newTargetPosition;
     }
 
-    public void periodic() {
-        if (elevatorMotorMain.getCurrentEncoderValue() < targetPosition && elevatorMotorMain.getCurrentEncoderValue() > targetPosition + DEADBAND_VALUE) {
-            elevatorMotorMain.set(motorSpeed, "Buttons said so.");
-        }
-        else if (elevatorMotorMain.getCurrentEncoderValue() > targetPosition && elevatorMotorMain.getCurrentEncoderValue() < targetPosition - DEADBAND_VALUE) {
-            elevatorMotorMain.set(-motorSpeed, "Buttons said so.");
-        }
+    public void setSpeed(double speed) {
+        if(Math.abs(speed) > 0.05)
+            elevatorMotorMain.set(speed, "Joystick said so");
+        else elevatorMotorMain.set(0, "Stopping elevator");
     }
+
+    // public void periodic() {
+    //     System.out.println("Target Pos: " + targetPosition);
+        
+    //     if (elevatorMotorMain.getCurrentEncoderValue() < targetPosition && elevatorMotorMain.getCurrentEncoderValue() > targetPosition + DEADBAND_VALUE) {
+    //         elevatorMotorMain.set(motorSpeed, "Buttons said so.");
+    //     }
+    //     else if (elevatorMotorMain.getCurrentEncoderValue() > targetPosition && elevatorMotorMain.getCurrentEncoderValue() < targetPosition - DEADBAND_VALUE) {
+    //         elevatorMotorMain.set(-motorSpeed, "Buttons said so.");
+    //     }
+    //     else {
+    //         elevatorMotorMain.set(0, "Stopping elevator");
+    //     }
+    // }
     
 }
