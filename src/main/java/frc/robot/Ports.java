21c596d7faf0f410b16a68de8d68a89f139e6021
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2023-03-13 17:51:29 -0400

    Added elevator sub motor

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -6,8 +6,8 @@ public class Ports {
    public final static int LEFT_DRIVE_FALCON_FRONT = 3;
    public final static int LEFT_DRIVE_FALCON_BACK = 1;
    public final static int INTAKE_MOTOR = 6;
-   public final static int ELEVATOR_MOTOR_MAIN = 18; //Normally 18
-   // public final static int ELEVATOR_MOTOR_SUB = 0;
+   public final static int ELEVATOR_MOTOR_MAIN = 18;
+   public final static int ELEVATOR_MOTOR_SUB = 19;
    public final static int PNEUMATICS_HUB = 5;
    public final static int CLAW_LEFT_CLOSE = 13; //Do these later
    public final static int CLAW_LEFT_OPEN = 15;
